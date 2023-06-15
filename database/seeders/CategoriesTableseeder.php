9f06bf1f7ce7d814d8e9d085638a65a06dc86463
yuya1453 <yuya19991@outlook.jp>
2023-06-15 13:45:21 +0900

    message

--- database/seeders/CategoriesTableseeder.php
+++ database/seeders/CategoriesTableseeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
-class CategoriesTableseeder extends Seeder
+class CategoriesTableSeeder extends Seeder
 {
     private $category;
     /**
