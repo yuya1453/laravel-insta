27915910ba0b23a7160ea2f70a1499aa0225e439
yuya1453 <yuya19991@outlook.jp>
2023-06-16 12:22:41 +0900

    updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CategoriesTableSeeder::class,
+            // CategoriesTableSeeder::class,
             UsersTableSeeder::class,
         ]);
     }
